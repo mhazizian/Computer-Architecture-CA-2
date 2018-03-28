16cea7d05709c69d234ab87bc604858fa83673f7
moridi <m.moridi.2009@gmail.com>
2018-03-29 00:07:14 +0430

    Fix SHIFTER and STACK bugs

--- shifter.sv
+++ shifter.sv
@@ -1,30 +1,59 @@
 `include "defines.sv"
 
-module shifter(data, opcode, shift_out, shift_count, Z, C);
+module shifter(data, opcode, shift_out, shift_count, Z, C, shift_enable);
 
-	input[7:0] data;
-	input[1:0] opcode;
+	input [7:0] data;
+	input [1:0] opcode;
+	input [2:0] shift_count;
+	input shift_enable;
+	
+	output logic Z, C;
 	output logic [7:0] shift_out;
-	shift_count, Z, C;
-
-	assign local_shifter = data
-	integer i;
-
-	always @(*) begin
+	
+	assign Z = (shift_out == 8'b0 ? 1 : 0);	
+	
+	always @(posedge shift_enable) begin
+	 
+		integer i;
+	
+		shift_out = data;
+		C = 0;
+		
 		case(opcode)
-		`SHL_FN:
-		begin
-			for (i = 0; i < shift_count; i = i + 1) begin	
-				local_shifter <= {local_shifter[
-		end
-		`SHR_FN: 
-		begin
-		end
-		`ROL_FN: 
-		begin
-		end
-		`ROR_FN: 
-		begin
-		end
+
+			`SHL_FN:
+			begin
+				for (i = 0; i < shift_count; i = i + 1) 
+				begin	
+					C = shift_out[7];
+					shift_out = {shift_out[6:0], 1'b0};
+				end
+			end
+			`SHR_FN:
+			begin
+				for (i = 0; i < shift_count; i = i + 1) 
+				begin
+					C = shift_out[0];
+					shift_out = {1'b0, shift_out[7:1]};
+				end
+			end
+			`ROL_FN:
+			begin
+				for (i = 0; i < shift_count; i = i + 1) 
+				begin	
+					shift_out = {shift_out[6:0], shift_out[7]}; 
+				end
+			end
+			
+			`ROR_FN: 
+			begin
+				for (i = 0; i < shift_count; i = i + 1) 
+				begin	
+					shift_out = {shift_out[0], shift_out[7:1]}; 
+				end
+			end
+			
 		endcase
 	end
+	
+endmodule
--- stack.sv
+++ stack.sv
@@ -1,21 +1,22 @@
 module Stack(address, push, pop, stack_out);
 
-	input[1:0] address;
+	input[11:0] address;
 	input push, pop;
-	output logic [1:0] stack_out;
+	output logic [11:0] stack_out;
 
-	reg [1:0] stack [0:3];
-	logic [1:0] pointer = 2'b0;
+	reg [11:0] stack [0:7];
+	logic [2:0] pointer = 3'b0;
+	
+	assign stack_out = stack[pointer];	
 	
 	always @(push, pop, address) begin	
 		if(pop)	begin
-			pointer <= pointer - 2'b01;
-			stack_out <= stack[pointer];			
+			pointer <= pointer - 3'b01;		
 		end
 
 		if(push) begin
 			stack[pointer] <= address;
-			pointer <= pointer + 2'b01;
+			pointer <= pointer + 3'b01;
 		end
 	end
 	
