289a22e40f805a62c101b268af3a8f6c8484b6ab
mhazizian <mohammadh.azizian@gmail.com>
2018-03-28 16:14:21 +0430

    Fixbug on instruction memory

--- instruction_memory.sv
+++ instruction_memory.sv
@@ -7,7 +7,7 @@ module InstructionMemory(rst, address, instruction);
 	reg[18:0] ins_memory[0:4095];
 
 	always @(posedge rst) begin
-		data <= '{default:19'b0};
+		ins_memory <= '{default:19'b0};
 		ins_memory[0] <= 19'b0000000101001100000;
 	end
 
