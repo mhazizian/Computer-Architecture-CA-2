af8f6642a254d3450561eccfcb0cbfd3dbfcf2f3
moridi <m.moridi.2009@gmail.com>
2018-03-28 12:12:12 +0430

    Ammend REGISTERS

--- registers.sv
+++ registers.sv
@@ -1,18 +1,32 @@
-module register_1(input clk, rst, ld, d, output logic q);
+module register_1(clk, rst, ld, d, q);
+
+	input clk, rst, ld, d;
+	output logic q;
+	
 	always@(posedge clk, posedge rst) begin
 		if (ld) q <= d;
 		if (rst) q <= 1'b0;
 	end
 endmodule
 
-module register_8(input clk, rst, ld,input [7:0]d, output logic [7:0]q);
+module register_8(clk, rst, ld, d, q);
+
+	input clk, rst, ld;
+	input [7:0]d;
+	output logic [7:0]q;
+
 	always@(posedge clk, posedge rst) begin
 		if (ld) q <= d;
 		if (rst) q <= 8'b0;
 	end
 endmodule
 
-module register_19(input clk, rst, ld,input [18:0]d, output logic [18:0]q);
+module register_19(clk, rst, ld, d, q);
+
+	input clk, rst, ld;
+	input [18:0]d;
+	output logic [18:0]q;
+
 	always@(posedge clk, posedge rst) begin
 		if (ld) q <= d;
 		if (rst) q <= 19'b0;
