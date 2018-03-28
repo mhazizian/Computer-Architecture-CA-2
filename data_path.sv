406fff7864cea42ff3f75df7293f79d5c022eabf
moridi <m.moridi.2009@gmail.com>
2018-03-28 18:33:48 +0430

    Add controller to data path

--- data_path.sv
+++ data_path.sv
@@ -4,9 +4,11 @@ module data_path(clk, rst);
 	
 	logic [11:0] next_pc, current_pc;
 	logic [18:0] instruction;
-	logic [7:0] alu_out, alu_in1, alu_in2;
-	logic cin = 1'b0, zero, cout;
+	logic [7:0] alu_out, alu_in1, alu_in2, register_file_out2;
+	logic cin = 1'b0, zero, cout, sel_ALUScr_reg, sel_ALUScr_const;
+	logic [2:0] ALU_op;
 	
+	Controller controller(instruction[18:13], ALU_op, sel_ALUScr_reg, sel_ALUScr_const);
 	
 	incrementer_12 inc_pc(current_pc, next_pc);
 
@@ -14,8 +16,10 @@ module data_path(clk, rst);
 	
 	InstructionMemory im(rst, current_pc, instruction);
 	
-	RegisterFile rf(clk, rst, instruction[10:8],  instruction[7:5], instruction[13:11], alu_out, alu_in1, alu_in2);
+	RegisterFile rf(clk, rst, instruction[10:8],  instruction[7:5], instruction[13:11], alu_out, alu_in1, register_file_out2);
 
+	mux_2_to_1_8 mux_alu_source(register_file_out2, instruction[7:0], sel_ALUScr_reg, sel_ALUScr_const, alu_in2);
+	
 	Alu alu(alu_in1, alu_in2, cin, instruction[16:14], alu_out, zero, cout);
 	
 endmodule
