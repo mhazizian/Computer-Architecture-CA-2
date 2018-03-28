8d7a8c9df13f8379624162cb8daadfbf7473b5b8
mhazizian <mohammadh.azizian@gmail.com>
2018-03-28 13:30:44 +0430

    Add DataMemory module and it's testbench

---
+++ data_memory.sv
@@ -0,0 +1,15 @@
+module DataMemory(rst, Address, WriteData, MemRead, MemWrite, ReadData);
+
+	input [7:0] Address, WriteData;
+	input rst, MemRead, MemWrite;
+	output logic[7:0] ReadData;
+
+	reg[7:0] data[0:255];
+
+	always @(rst, Address, MemRead, MemWrite) begin
+		if (rst) data <= '{default:8'b0};
+		if (MemRead) ReadData = data[Address];
+		if (MemWrite) data[Address] = WriteData;
+	end // always @(Address, MemRead, MemWrite)
+
+endmodule
