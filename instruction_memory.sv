f2326b43fd9050aa08ae8b9490133fed40eef019
mhazizian <mohammadh.azizian@gmail.com>
2018-03-28 14:58:58 +0430

    Add instuction_memory module

---
+++ instruction_memory.sv
@@ -0,0 +1,18 @@
+module InstructionMemory(rst, Address, Instruction);
+
+	input [7:0] Address;
+	input rst;
+	output logic[18:0] Instruction;
+
+	reg[18:0] ins_memory[0:255];
+
+	always @(posedge rst) begin
+		data <= '{default:19'b0};
+		Instruction[0] <= 19'b0000000101001100000;
+	end
+
+	always @(Address) begin
+		Instruction <= ins_memory[Address];
+	end
+
+endmodule
