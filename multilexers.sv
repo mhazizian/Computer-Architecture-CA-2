0a8d0d98517b270f8a9bfb09192353b4b364e68d
mhazizian <mohammadh.azizian@gmail.com>
2018-03-28 18:18:33 +0430

    Amend multiplexers input-output style

--- multilexers.sv
+++ multilexers.sv
@@ -1,11 +1,20 @@
-module mux_2_to_1_8(input[7:0] first, second, input sel_first, sel_second, output[7:0] out);
-  assign out = sel_first ? first : (sel_second ? second : out); 
+module mux_2_to_1_8(first, second, sel_first, sel_second, out);
+	input[7:0] first, second;
+	input sel_first, sel_second;
+	output[7:0] out;
+	assign out = sel_first ? first : (sel_second ? second : out); 
 endmodule
 
-module mux_3_to_1_8(input[7:0] first, second, third, input sel_first, sel_second, sel_third, output[7:0] out);
-  assign out = sel_first ? first : (sel_second ? second : (sel_third ? third : out)); 
+module mux_3_to_1_8(first, second, third, sel_first, sel_second, sel_third, out);
+	input[7:0] first, second, third;
+	input sel_first, sel_second, sel_third;
+	output[7:0] out;
+	assign out = sel_first ? first : (sel_second ? second : (sel_third ? third : out)); 
 endmodule
 
-module mux_2_to_1_1(input first, second, input sel_first, sel_second, output out);
-  assign out = sel_first ? first : (sel_second ? second : out); 
+module mux_2_to_1_1(first, second, sel_first, sel_second, out);
+	input first, second;
+	input sel_first, sel_second;
+	output out;
+	assign out = sel_first ? first : (sel_second ? second : out); 
 endmodule
