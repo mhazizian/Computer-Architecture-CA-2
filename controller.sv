26f99a500442ec37a440b502f9ce7ef0f05bb806
mhazizian <mohammadh.azizian@gmail.com>
2018-03-28 19:30:05 +0430

    Add NO_CONDITIONAL_JUMP_TYPE_OPCODE to controller

--- controller.sv
+++ controller.sv
@@ -1,13 +1,14 @@
 `include "defines.sv"
 
-module Controller(instruction, ALU_op, sel_ALUScr_reg, sel_ALUScr_const);
+module Controller(instruction, ALU_op, sel_ALUScr_reg, sel_ALUScr_const, sel_PCSCrc_const);
 
 	input [5:0] instruction;
 	output logic[2:0] ALU_op;
-	output logic sel_ALUScr_reg, sel_ALUScr_const;
+	output logic sel_ALUScr_reg, sel_ALUScr_const, sel_PCSCrc_const;
 
 	always @(instruction) begin
 		sel_ALUScr_reg = 0; sel_ALUScr_const = 0;
+		sel_PCSCrc_const = 0;
 
 		if (instruction[5:4] ==`REGISTER_TYPE_OPCODE) begin
 			ALU_op = instruction[3:0];
@@ -16,6 +17,9 @@ module Controller(instruction, ALU_op, sel_ALUScr_reg, sel_ALUScr_const);
 		if (instruction[5:4] ==`IMMEDIATE_TYPE_OPCODE) begin
 			ALU_op = instruction[3:0];
 			sel_ALUScr_const = 1;
+		end
+		if (instruction[5:2] ==`NO_CONDITIONAL_JUMP_TYPE_OPCODE) begin
+			sel_PCSCrc_const = 1;
 		end
 			
 	end
