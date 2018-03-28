d734dcef8717b99fe96d482f5455059a269980aa
mhazizian <mohammadh.azizian@gmail.com>
2018-03-28 19:06:13 +0430

    Add example for cuncorency bug

--- instruction_memory.sv
+++ instruction_memory.sv
@@ -8,12 +8,15 @@ module InstructionMemory(rst, address, instruction);
 
 	always @(posedge rst) begin
 		ins_memory <= '{default:19'b0};
-		ins_memory[2] <= 19'b0100000100000001010;
-		ins_memory[3] <= 19'b0101001000100000101;
-		ins_memory[4] <= 19'b0111001100000010001;
-		ins_memory[5] <= 19'b0010100101000001010;
-		ins_memory[6] <= 19'b0110110100100000101;
-				
+
+		ins_memory[1] <= 19'b0000000100101000000;
+		ins_memory[1] <= 19'b0000000100101000000;
+		ins_memory[1] <= 19'b0000000100101000000;
+		ins_memory[1] <= 19'b0000000100101000000;
+		ins_memory[1] <= 19'b0000000100101000000;
+		ins_memory[1] <= 19'b0000000100101000000;
+		ins_memory[1] <= 19'b0000000100101000000;
+		ins_memory[1] <= 19'b0000000100101000000;
 	end
 
 	always @(address) begin
