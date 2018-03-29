d7b31364a675585dfb89029484036e0e667f72f4
mhazizian <mohammadh.azizian@gmail.com>
2018-03-29 14:02:50 +0430

    Del sel_RegisterFileWriteDst_r2 signal

--- controller.sv
+++ controller.sv
@@ -2,7 +2,7 @@
 
 module Controller(instruction, ALU_op, sel_ALUScr_reg, sel_ALUScr_const,
 	sel_PCSrc_offset, sel_PCSrc_const, sel_PCSrc_plus1, MemWrite, MemRead,
-	sel_RegisterFile_in_alu, sel_RegisterFile_in_memory, RegisterFileWriteEn, sel_RegisterFileWriteDst_r2,
+	sel_RegisterFile_in_alu, sel_RegisterFile_in_memory, RegisterFileWriteEn,
 	sel_RegisterFile_in_shifter, sel_RegisterFileReadReg2_rd);
 
 	input [5:0] instruction;
@@ -10,7 +10,7 @@ module Controller(instruction, ALU_op, sel_ALUScr_reg, sel_ALUScr_const,
 	output logic sel_ALUScr_reg, sel_ALUScr_const,
 		sel_PCSrc_const, sel_PCSrc_offset, sel_PCSrc_plus1,
 		MemWrite, MemRead, sel_RegisterFile_in_alu, sel_RegisterFile_in_memory,
-		RegisterFileWriteEn, sel_RegisterFileWriteDst_r2, sel_RegisterFile_in_shifter,
+		RegisterFileWriteEn, sel_RegisterFile_in_shifter,
 		sel_RegisterFileReadReg2_rd;
 
 	always @(instruction) begin
@@ -21,7 +21,6 @@ module Controller(instruction, ALU_op, sel_ALUScr_reg, sel_ALUScr_const,
 		sel_RegisterFile_in_alu = 0;
 		sel_RegisterFile_in_memory = 0;
 		RegisterFileWriteEn = 0;
-		sel_RegisterFileWriteDst_r2 = 0;
 		sel_RegisterFile_in_shifter = 0;
 		sel_RegisterFileReadReg2_rd = 0;
 
@@ -57,7 +56,6 @@ module Controller(instruction, ALU_op, sel_ALUScr_reg, sel_ALUScr_const,
 				MemRead = 1;
 				sel_RegisterFile_in_memory = 1;
 				RegisterFileWriteEn = 1;
-				sel_RegisterFileWriteDst_r2 = 1;
 			end
 
 		end
