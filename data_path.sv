d37b99d54ba16e60dc3e05510fc0ee0e5a7c689f
mhazizian <mohammadh.azizian@gmail.com>
2018-03-29 14:46:43 +0430

    Fixbug on using controller in datapath

--- data_path.sv
+++ data_path.sv
@@ -19,7 +19,7 @@ module data_path(clk, rst);
 	
 	// Controller
 
-	Controller controller(instruction, ALU_op, sel_ALUScr_reg, sel_ALUScr_const,
+	Controller controller(instruction[18:13], ALU_op, sel_ALUScr_reg, sel_ALUScr_const,
 	sel_PCSrc_offset, sel_PCSrc_const, sel_PCSrc_plus1, MemWrite, MemRead,
 	sel_RegisterFile_in_alu, sel_RegisterFile_in_memory, RegisterFileWriteEn,
 	sel_RegisterFile_in_shifter, sel_RegisterFileReadReg2_rd);
