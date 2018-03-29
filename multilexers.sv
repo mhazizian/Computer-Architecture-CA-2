2d0cb5f0b86ad54671c0e3e1973e60489d13a1d7
moridi <m.moridi.2009@gmail.com>
2018-03-29 09:14:55 +0430

    Fix bug multiplexers

--- multilexers.sv
+++ multilexers.sv
@@ -2,7 +2,7 @@ module mux_2_to_1_8(first, second, sel_first, sel_second, out);
 
 	input[7:0] first, second;
 	input sel_first, sel_second;
-	output[7:0] out;
+	output logic [7:0] out;
 	
 	assign out = sel_first ? first : (sel_second ? second : out); 
 	
@@ -12,7 +12,7 @@ module mux_3_to_1_8(first, second, third, sel_first, sel_second, sel_third, out)
 
 	input[7:0] first, second, third;
 	input sel_first, sel_second, sel_third;
-	output[7:0] out;
+	output logic [7:0] out;
 	
 	assign out = sel_first ? first : (sel_second ? second : (sel_third ? third : out)); 
 	
@@ -22,7 +22,7 @@ module mux_2_to_1_3(first, second, sel, out);
 
 	input[2:0] first, second;
 	input sel;
-	output[2:0] out;
+	output logic [2:0] out;
 	
 	assign out = sel ? first : second; 
 	
@@ -32,7 +32,7 @@ module mux_2_to_1_1(first, second, sel_first, sel_second, out);
 
 	input first, second;
 	input sel_first, sel_second;
-	output out;
+	output logic out;
 	
 	assign out = sel_first ? first : (sel_second ? second : out); 
 	
@@ -42,7 +42,7 @@ module mux_3_to_1_12(first, second, third, sel_first, sel_second, sel_third, out
 
 	input[11:0] first, second, third;
 	input sel_first, sel_second, sel_third;
-	output[11:0] out;
+	output logic [11:0] out;
 	
 	assign out = sel_first ? first : (sel_second ? second : (sel_third ? third : out));  
 	
