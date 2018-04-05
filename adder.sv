c88ec49c1d035dd01f78aa959ae3946b84b700b4
moridi <m.moridi.2009@gmail.com>
2018-04-05 10:24:06 +0430

    Add add_sub

--- adder.sv
+++ adder.sv
@@ -6,3 +6,13 @@ module adder_12(first, second, sum);
 	assign sum = first + second;
 
 endmodule
+
+module add_sub_12(first, second, res);
+	
+	input[11:0] first;
+	input[7:0] second;
+	output logic [11:0] res;
+		
+	assign res = second[7] ? (first - {5'b0, second[6:0]}) : (first + {5'b0, second[6:0]});
+
+endmodule
--- data_path.sv
+++ data_path.sv
@@ -2,7 +2,7 @@ module data_path(clk, rst);
 
 	input clk, rst;
 	
-	logic [11:0] next_pc, current_pc, pc_plus1, pc_offset, pc_plus_offset, stack_out;
+	logic [11:0] next_pc, current_pc, pc_plus1, pc_plus_offset, stack_out;
 	logic [18:0] instruction;
 	
 	logic [7:0] alu_out, alu_in1, alu_in2, register_file_out2, data_memory_out,
@@ -30,12 +30,6 @@ module data_path(clk, rst);
 	sel_RegisterFile_in_alu, sel_RegisterFile_in_memory, RegisterFileWriteEn,
 	sel_RegisterFile_in_shifter, sel_RegisterFileReadReg2_rd, sel_Cin_alu, sel_Cin_shifter,
 	push_stack, pop_stack);
-
-	
-	// Sign enxtender
-	
-	sign_extender s_extender(instruction[7:0], pc_offset);
-	
 	
 	// Stack block
 	
@@ -44,55 +38,38 @@ module data_path(clk, rst);
 	// PC block
 	
 	incrementer_12 inc_pc(current_pc, pc_plus1);
-	
-	adder_12 pc_adder(pc_offset, pc_plus1, pc_plus_offset);
+
+	add_sub_12 pc_adder(pc_plus1, instruction[7:0], pc_plus_offset);
 
 	mux_4_to_1_12 mux_pc_source(pc_plus1, instruction[11:0], pc_plus_offset, stack_out, sel_PCSrc_plus1, sel_PCSrc_const, sel_PCSrc_offset, sel_PCSrc_stack, next_pc);
 	
 	pc_register pc(clk, rst, next_pc, current_pc);	
 	
-	
-	
 	// Instruction memory
 	
-	InstructionMemory im(rst, current_pc, instruction);
-	
-	
+	InstructionMemory im(rst, current_pc, instruction);	
 	
 	// Register file
-	
-//	mux_2_to_1_3 mux_rf_write_source(instruction[10:8], instruction[13:11], sel_RegisterFileWriteDst_r2, register_file_write_dst);
 	
 	mux_2_to_1_3 mux_rf_second_source(instruction[13:11], instruction[7:5], sel_RegisterFileReadReg2_rd, register_file_reg2_input);
 	
 	RegisterFile rf(clk, rst, RegisterFileWriteEn, instruction[10:8], register_file_reg2_input, instruction[13:11], register_file_write_input, alu_in1, register_file_out2);
 
-	
-	
 	// ALU block
 	
 	mux_2_to_1_8 mux_alu_source(register_file_out2, instruction[7:0], sel_ALUScr_reg, sel_ALUScr_const, alu_in2);
-	
-//	Alu alu(alu_in1, alu_in2, cin, instruction[16:14], alu_out, Z, cout);
 
 	Alu alu(alu_in1, alu_in2, C_out, instruction[16:14], alu_out, C_in_alu, Z_in_alu);
-	
-	
-	
+		
 	// Shifter
 		
-	shifter shifter_block(alu_in1, instruction[15:14], shift_out, instruction[7:5], C_in_shifter, Z_in_shifter);//, shift_enable);
-
-//  shift_out added
-	
-	
+	shifter shifter_block(alu_in1, instruction[15:14], shift_out, instruction[7:5], C_in_shifter, Z_in_shifter);//, shift_enable);	
 	
 	// Data Memory block
 	
 	DataMemory data_memory(rst, alu_out, register_file_out2, MemRead, MemWrite, data_memory_out);
 
 	mux_3_to_1_8 mux_data_memory(data_memory_out, alu_out, shift_out, sel_RegisterFile_in_memory, sel_RegisterFile_in_alu, sel_RegisterFile_in_shifter, register_file_write_input);
-
 
 	// Z flip flop
 	
